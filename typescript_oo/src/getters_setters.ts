1203f981d22212fd90aca8f09c6b98c2f861502c
CelioVilela <celiocvilela70@gmail.com>
2025-10-18 14:49:21 -0300

    commit

--- typescript_oo/src/getters_setters.ts
+++ typescript_oo/src/getters_setters.ts
@@ -1,21 +1,37 @@
 export class Address {  
-  private address: string = '';
-  private zipCode: string = '';
-  private number?: number
+  private _address: string = '';
+  private _zipCode: string = '';
+  private _numberA?: number
 
-  public setAddress(address: string): void {
+  public set address(address: string){
     if (address.length < 3){
       throw new Error('invalid address')
     }
-    this.address = address
+    this._address = address
   }
 
-  public getAddress(): string {
-    return this.address
+  public get address(): string {
+    return this._address
+  }
+
+  public set zipCode(zipCode: string){
+    this._zipCode = zipCode
+  }
+
+  public get zipCode(): string {
+    return this._zipCode.replace(/\D/g, '')
+  }
+
+  public set numberA(numberA: number){
+    this._numberA = numberA
+  }
+
+  public get numberA(): number {
+    return this._numberA ?? 123
   }
 }
-
 const address1 = new Address()
-console.log(address1.getAddress())
-address1.setAddress('Rua xx')
-console.log(address1.getAddress())
+address1.address = 'Rua nm'
+address1.zipCode = '55297-090'
+address1.numberA = 12
+console.log(address1.address,address1.zipCode,address1.numberA)
